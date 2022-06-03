6e94af75f04b2642dbad9af4e00073923d18553c
Wajih Belhadj <bel.wajih@gmail.com>
2022-06-03 02:01:49 +0100

    ReactState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import './App.css'
+import myPhoto from './Photo.jpg'
+class App extends React.Component{
+  state = {
+    fullName: "Wajih Belhadj",
+    bio: "Sousse",
+    imgSrc: myPhoto,
+    profession: "Mechatronics engineer  and  front-end developer",
+    show: true
+  }
+  handleClick = (state) => {
+    this.setState({ ...state, show: !this.state.show })
+    console.log('cliked!')
+  }
+  render(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    if (this.state.show == true) {
+      return (
+        <>
+          <div className="card-container">
+          <button className="button-63" onClick={this.handleClick}> Hide</button>
+            <img className="round" src={this.state.imgSrc} alt="user" />
+            <h3>{this.state.fullName}</h3>
+            <h6>{this.state.bio}</h6>
+            <p>{this.state.profession}</p>
+            <div className="buttons">
+              <button className="primary">
+                Message
+              </button>
+              <button className="primary ghost">
+                Following
+              </button>
+            </div>
+            <div className="skills">
+              <h6>Skills</h6>
+              <ul>
+                <li>UI / UX</li>
+                <li>Front End Development</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node</li>
+              </ul>
+            </div>
+          </div>
+        
+        </>
+      )
+    } else { return (<>
+      <button className="button-63" onClick={this.handleClick}> Show</button>
+      <div>none!</div>
+      </>) }
+  }
 }
-
-export default App;
+export default App
